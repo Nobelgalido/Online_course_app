01af28aaebe2a916c3df515fac83c8adb0d8b4a8
NobelGalido <galidoalfred@gmail.com>
2025-11-26 18:20:24 +0800

    Add Question, Choice, and Submission models with admin configuration

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -3,30 +3,39 @@ from django.contrib import admin
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Submission)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline]
+    inlines = [LessonInline, QuestionInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-
 # <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ('content', 'course', 'grade')
+    list_filter = ['course']
+    search_fields = ['content']
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
